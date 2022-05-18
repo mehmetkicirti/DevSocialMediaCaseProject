a3fdb56f0acce6aae26978e5cdf021ce5dd7697a
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-18 23:46:26 +0300

    FluentValidation Package has been injected to validate entities.

---
+++ src/API/Common/DevSocialMediaCaseProject.Common/Aspects/Validation/ValidationAspect.cs
@@ -0,0 +1,35 @@
+﻿using Castle.DynamicProxy;
+using DevSocialMediaCaseProject.Common.Constants;
+using DevSocialMediaCaseProject.Common.CrossCuttingConcerns.Validation;
+using DevSocialMediaCaseProject.Common.Utilities.Interceptors;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevSocialMediaCaseProject.Common.Aspects.Validation
+{
+    public class ValidationAspect : MethodInterceptor
+    {
+        private Type _validatorType;
+        public ValidationAspect(Type validatorType)
+        {
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+                throw new Exception(ExceptionConstants.VALIDATION_TYPE_ERROR);
+            _validatorType = validatorType;
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            var entityType = _validatorType.BaseType.GetGenericArguments()[0]; // for ex. => AbstractValidator<Blog> => Blog generic Arguments of first item
+            var entities = invocation.Arguments.Where(arg => arg.GetType() == entityType); // It is getting equal to given type while sending request.
+
+            foreach (var entity in entities)
+            {
+                ValidationTool.ValidateAsync(validator, entity).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
---
+++ src/API/Common/DevSocialMediaCaseProject.Common/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSocialMediaCaseProject.Common.CrossCuttingConcerns.Validation
+{
+    public class ValidationTool
+    {
+        public async static Task ValidateAsync(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var results = await validator.ValidateAsync(context);
+            if (!results.IsValid)
+                throw new ValidationException(results.Errors);
+        } 
+    }
+}
