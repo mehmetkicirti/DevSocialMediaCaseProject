3c17cfd3e4e3a5159db8ac7b6d3bcd0f1c8e857d
mehmetkicirti <mehmetkicirti@hotmail.com>
2022-05-19 15:27:31 +0300

    Swagger auth options added and also service registeration took into CommonModule.

--- src/API/Common/DevSocialMediaCaseProject.Common/Extensions/CommonRegistrationExtension.cs
+++ src/API/Common/DevSocialMediaCaseProject.Common/Extensions/CommonRegistrationExtension.cs
@@ -6,8 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.OpenApi.Models;
 
 namespace DevSocialMediaCaseProject.Common.Extensions
 {
@@ -36,6 +35,41 @@ namespace DevSocialMediaCaseProject.Common.Extensions
                         IssuerSigningKey = CredentialsHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "1.0.0",
+                    Title = "API Swagger",
+                    Description = "Api Swagger Documentation",
+                    TermsOfService = new Uri("http://swagger.io/terms/"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Mehmet Aydın KICIRTI",
+                        Email = "mehmetkicirti@hotmail.com",
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjYyODU5NTY3MWMxNzljZGM1MDdhZWM5YSIsImVtYWlsIjoibWVobWV0a2ljaXJ0aUBnbWFpbC5jb20iLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiTWVobWV0ICRLSUNJUlRJIiwibmJmIjoxNjUyOTMxMDQzLCJleHAiOjE2NTI5MzE5NDMsImlzcyI6Ii9udHRkYXRhdGVzdGNhc2UiLCJhdWQiOiIvbnR0ZGF0YXRlc3RjYXNlIn0.8rC35jEQNh0FL9fFTXtJV4EKJkd0ei6A2v_S2xMDsz0\"",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
         }
     }
 }
--- src/API/Presentation/DevSocialMediaCaseProject.WebAPI/Startup.cs
+++ src/API/Presentation/DevSocialMediaCaseProject.WebAPI/Startup.cs
@@ -1,4 +1,3 @@
-using DevSocialMediaCaseProject.Common.Configurations.MongoDB;
 using DevSocialMediaCaseProject.Common.Extensions;
 using DevSocialMediaCaseProject.Persistence;
 using Microsoft.AspNetCore.Builder;
@@ -24,25 +23,10 @@ namespace DevSocialMediaCaseProject.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson(opt=> opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+            services.AddControllers().AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
             services.AddCors();
             services.AddPersistenceServiceRegistration();
             services.AddCommonServicesRegistration(Configuration);
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "1.0.0",
-                    Title = "API Swagger",
-                    Description = "Api Swagger Documentation",
-                    TermsOfService = new Uri("http://swagger.io/terms/"),
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Mehmet Aydýn KICIRTI",
-                        Email = "mehmetkicirti@hotmail.com",
-                    }
-                });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,13 +38,13 @@ namespace DevSocialMediaCaseProject.WebAPI
             }
 
             app.ConfigurateCustomExceptionMiddleware();
-            
+
             app.UseCors(builder => builder.WithOrigins("http://localhost:3000", "http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            
+
             app.UseAuthentication();
 
             app.UseAuthorization();
